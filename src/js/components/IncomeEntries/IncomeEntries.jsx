a4b28e746d237462206dbbadccb6d46d8541b022
Abby <abigailsy1@yahoo.com>
2018-08-15 13:19:37 -0700

    updated commits

--- src/js/components/IncomeEntries/IncomeEntries.jsx
+++ src/js/components/IncomeEntries/IncomeEntries.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-export default class incomeEntries extends React.Component {
+export default class IncomeEntries extends React.Component {
     constructor(props) {
         super(props);
     }
-
     render() {
         return (
-            <div className='card border-success mb-3'>
+            <div className='card boder-success mb-3'>
+                <div className='card-header text-white bg-success'>income Entries</div>
                 <div className='card-body'>
                     <form>
                         <div className='form-group'>
@@ -17,18 +17,17 @@ export default class incomeEntries extends React.Component {
 
                         <div className='form-group'>
                             <label htmlFor='income-amount'>Amount</label>
-                            <div className='input-group'>
-                                <span className='input-group-addon'>$</span>
-                                <input type='text' className='form-control' id='income-amount' />
-                            </div>
+                            <span className='input-group-addon'>$</span>
+                            <input type='text' className='form-control' id='income-amount' />
                         </div>
 
                         <button type='button' className='btn btn-success col-12 mb-5'>+ Add Income</button>
+
                         <table className='table table-sm table-hover'>
                             <thead>
                                 <tr>
-                                    <th>Description</th>
-                                    <th style={{ width: 120 }}>Amount</th>
+                                    <th>Desciprtion:</th>
+                                    <th style ={{ width: 120 }}>Amount</th>
                                 </tr>
                             </thead>
                             <tbody>
--- src/js/rootStore.js
+++ src/js/rootStore.js
@@ -1,12 +1,9 @@
 import { createStore } from 'redux';
+import rootReducer from './rootReducer';
 
-function tempReducer () {
-    return null;
-}
-
-const rootStore = createStore (
-    tempReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const rootStore = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 export default rootStore;
