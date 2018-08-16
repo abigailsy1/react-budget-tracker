6464968bad9301edf0fef5984a868b5d419ac82d
Abby <abigailsy1@yahoo.com>
2018-08-15 19:25:19 -0700

    make it better

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
-/* eslint no-console: "off" */
-const server = require('./app');
+const server = require('./server');
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
+server.listen(PORT, () => console.log(`Server is listening on http://127.0.0.1:${PORT}`));
